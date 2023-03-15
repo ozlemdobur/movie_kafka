daa3623a9fb50febf4cdd05bb44e295ee721bd36
Ozlem <doburozlem@gmail.com>
2023-03-15 13:15:39 +0100

    with Serilization error

--- comment-processor/src/main/java/org/acme/MovieCommentDTO.java
+++ comment-processor/src/main/java/org/acme/MovieCommentDTO.java
@@ -1,10 +1,31 @@
 package org.acme;
+
+@RegisterForReflection
 public class MovieCommentDTO {
     private Long movieId;
     private String comment;
 
+    public MovieCommentDTO() {
+    }
+
     public MovieCommentDTO(Long movieId, String comment) {
         this.movieId = movieId;
+        this.comment = comment;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
         this.comment = comment;
     }
 
--- movie-processor/src/main/java/org/acme/dto/MovieCommentDTO.java
+++ movie-processor/src/main/java/org/acme/dto/MovieCommentDTO.java
@@ -1,10 +1,15 @@
 package org.acme.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class MovieCommentDTO {
     private Long id;
     private Long movieId;
