fdf6b4fc599e2444c86efda19ec8fec7e20ede2d
Ozlem <doburozlem@gmail.com>
2023-03-16 15:37:31 +0100

    Director Resource test is finished

---
+++ movie-processor/src/test/java/org/acme/resource/DirectorResourceTest.java
@@ -0,0 +1,19 @@
+package org.acme.resource;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectorResourceTest {
+
+    @Test
+    @Disabled
+    void save() {
+    }
+
+    @Test
+    @Disabled
+    void findById() {
+    }
+}
